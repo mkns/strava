3930c0059c2e804f1a2bac878f91fd435052d9e0
Kenny Scott <kenny@kennyscott.co.uk>
2020-06-01 16:43:19 +0100

    adding base walk app that does nothing

--- strava/urls.py
+++ strava/urls.py
@@ -18,5 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('getactivitykudos/', include('getactivitykudos.urls')),
+    path('walk/', include('walk.urls')),
     path('admin/', admin.site.urls),
 ]
